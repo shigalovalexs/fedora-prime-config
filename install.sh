38f2f28eb356aa9a151f7942372ca5dd18810cb5
Shigalov Alexey <shigalovalexs@gmail.com>
2023-06-04 00:14:38 +0300

    ...

--- install.sh
+++ install.sh
@@ -13,12 +13,9 @@ dconf update
 echo "Install Visual Studio Code and WineHQ repository"
 wget -O /etc/yum.repos.d/vscode.repo https://packages.microsoft.com/yumrepos/vscode/config.repo
 wget -O /etc/yum.repos.d/winehq.repo https://dl.winehq.org/wine-builds/fedora/$(rpm -E %fedora)/winehq.repo
-dnf -y check-update
 
 echo "Install RPM Fusion repository"
 dnf -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-dnf -y check-update
-dnf -y groupupdate core
 
 echo "Install TLP"
 dnf -y install tlp tlp-rdw
