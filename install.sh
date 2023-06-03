9dfc45fb02e52481d1b022458b2b039130793372
Shigalov Alexey <shigalovalexs@gmail.com>
2023-06-03 23:02:00 +0300

    add RPM Fusion repository, install multimedia codecs

--- install.sh
+++ install.sh
@@ -10,17 +10,28 @@ tar -xvf /tmp/Bibata-Original-Ice.tar.gz -C /usr/share/icons
 echo "Update system databases"
 dconf update
 
-echo "Update package cache"
-dnf check-update
+echo "Install Visual Studio Code and WineHQ repository"
+wget -O /etc/yum.repos.d/vscode.repo https://packages.microsoft.com/yumrepos/vscode/config.repo
+wget -O /etc/yum.repos.d/winehq.repo https://dl.winehq.org/wine-builds/fedora/$(rpm -E %fedora)/winehq.repo
+dnf -y check-update
+
+echo "Install RPM Fusion repository"
+dnf -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+dnf -y check-update
+dnf -y groupupdate core
 
 echo "Install TLP"
-dnf install tlp tlp-rdw --assumeyes
-dnf remove power-profiles-daemon --assumeyes
+dnf -y install tlp tlp-rdw
+dnf -y remove power-profiles-daemon
 systemctl enable --now tlp.service
 systemctl mask systemd-rfkill.service systemd-rfkill.socket
 
 echo "Install Visual Studio Code and WineHQ"
-dnf install code winehq-stable --assumeyes
+dnf -y install code winehq-stable
 
 echo "Install Console"
-dnf install gnome-console --assumeyes
+dnf -y install gnome-console
+
+echo "Install multimedia codecs"
+dnf -y swap ffmpeg-free ffmpeg --allowerasing
+dnf -y install intel-media-driver
